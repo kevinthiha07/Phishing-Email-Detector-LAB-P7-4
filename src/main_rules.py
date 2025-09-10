a043a47ffb7964e58fabc348da2d93ff1e948e2c
Kevin Thiha <kevinyan200307@gmail.com>
2025-09-10 11:50:47 +0800

    Edited main_rules

--- src/main_rules.py
+++ src/main_rules.py
@@ -1,16 +1,18 @@
-# from whitelist import whitelist_score
-# from keywords import keyword_score
-# from keyword_position import keyword_position_score
-# from edit_distance import edit_distance_score
-# from url import url_score
+from whitelist import whitelist_score
+from keywords import keyword_score
+from keyword_position import keyword_position_score
+from edit_distance_check import edit_distance_score
+from url_detection import url_score
+from final_risk_scores import final_risk_score
 
 def score_email(email):
     scores = {
         "whitelist": whitelist_score(email),
-        "keywords": keyword_score(email),
+        "keyword_detection": keyword_score(email),
         "keyword_position": keyword_position_score(email),
         "edit_distance": edit_distance_score(email),
-        "url": url_score(email),
+        "url_detection": url_score(email),
+        "final_risk_score": final_risk_score(email),
     }
 
     # final = sum(scores.values())
