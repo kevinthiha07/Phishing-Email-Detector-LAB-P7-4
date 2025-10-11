ea31e05b0c6a3ce776dd8b57970243e4363ba89f
glenda <glendateo233@gmail.com>
2025-10-11 14:45:04 +0800

    safe domains

--- src/whitelist.py
+++ src/whitelist.py
@@ -41,8 +41,6 @@ def is_domain_whitelisted(email, domains, tlds):
 def load_whitelist():
     #whitelisted domains and top level domain
     return (
-        "netflix.com", "dropbox.com", "adobe.com", "salesforce.com",
-        "spotify.com", "zoom.us", "skype.com",
         {"microsoft","google","apple","amazon","paypal","linkedin","gmail", "yahoo", "outlook", "hotmail", "mail","aol","icloud","protonmail","zoho","msn","facebook","twitter","instagram","whatsapp","telegram","visa","mastercard","americanexpress","wellsfargo","bankofamerica","chase","citibank","netflix","dropbox","adobe","salesforce","spotify","zoom","skype","web","gmx","t-online","naver","daum","qq","163","rediff","ida","freemail","msg","gp","ebay","grenet","cwie","tfn","linux","263","napster","cutey","patra","hotp","ba"}, 
         {"com", "org", "net", "edu", "gov", "co","de","ru","lv","no",".fr"}          
     )
